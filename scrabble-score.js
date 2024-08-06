5cfc9b8a3f5b28a9b01e46259ef8c66246cf20c4
Greedent <frankkeith63@gmail.com>
2024-08-06 14:27:51 +0100

    Ticket 2 start

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,64 @@
-export function calculateScrabbleScore(word) {}
+// let A = 1
+// let B = 3
+// let C = 3
+// let D = 2
+// let E = 1
+// let F = 4
+// let G = 2
+// let H = 4
+// let I = 1
+// let J = 8
+// let K = 5
+// let L = 1
+// let M = 3
+// let N = 1
+// let O = 1
+// let P = 3
+// let Q = 10
+// let R = 1
+// let S = 1
+// let T = 1
+// let U = 1
+// let V = 4
+// let W = 4
+// let X = 8
+// let Y = 4
+// let Z = 10
+
+export function calculateScrabbleScore(input) {
+    const letterScores = {
+        a: 1,
+        e: 1,
+        i: 1,
+        o: 1,
+        u: 1,
+        l: 1,
+        n: 1,
+        r: 1,
+        s: 1,
+        t: 1,
+        d: 2,
+        g: 2,
+        b: 3,
+        c: 3,
+        m: 3,
+        p: 3,
+        f: 4,
+        h: 4,
+        v: 4,
+        w: 4,
+        y: 4,
+        k: 5,
+        j: 8,
+        x: 8,
+        q: 10,
+        z: 10,
+      };
+      
+      const scrabbleScore = input =>
+        // input
+        //   .split('')
+          input.map(letter => letterScores[letter])
+          return input
+        //   .reduce((a, b) => a + b);
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,5 +1,6 @@
-import { calculateScrabbleScore } from "./scrabble-score";
+import { calculateScrabbleScore, input, letterScores, scrabbleScores} from "./scrabble-score.js";
+import { expect, test } from 'vitest';
 
 test('calculate scrabble score for A', () => {
-    expect(calculateScrabbleScore(A)).toBe(1);
+    expect(calculateScrabbleScore('a')).toBe(1);
  })
