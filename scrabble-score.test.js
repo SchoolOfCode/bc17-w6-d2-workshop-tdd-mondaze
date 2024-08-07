f56ff4cd4e1d23be5c9f33f58f0a9fd8c258483a
Lana Maugé <lanamauge@gmail.com>
2024-08-07 09:38:17 +0100

    Ticket 2 optional task

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,15 +1,38 @@
 import { calculateScrabbleScore, input, letterScores, scrabbleScores} from "./scrabble-score.js";
 import { expect, test } from 'vitest';
 
-test('calculate scrabble score for A', () => {
-    expect(calculateScrabbleScore('a')).toBe(1);
- })
+// test('calculate scrabble score for A', () => {
+//     expect(calculateScrabbleScore('a')).toBe(1);
+//  })
 
- test('calculate scrabble score for longer words (a)', () => {
-    expect(calculateScrabbleScore('celestial')).toBe(11);
-    expect(calculateScrabbleScore('azumarill')).toBe(20);
-    expect(calculateScrabbleScore('ambivalent')).toBe(17);
-    expect(calculateScrabbleScore('clefairy')).toBe(16);
-    expect(calculateScrabbleScore('unbeknownst')).toBe(20);
- })
+//  test('calculate scrabble score for longer words (a)', () => {
+//     expect(calculateScrabbleScore('celestial')).toBe(11);
+//     expect(calculateScrabbleScore('azumarill')).toBe(20);
+//     expect(calculateScrabbleScore('ambivalent')).toBe(17);
+//     expect(calculateScrabbleScore('clefairy')).toBe(16);
+//     expect(calculateScrabbleScore('unbeknownst')).toBe(20);
+//  })
+
+
+// Define test cases
+const testCases = [
+   ['a', 1],
+   ['b', 3],
+   ['celestial', 11],
+   ['azumarill', 20],
+   ['ambivalent', 17],
+   ['clefairy', 16],
+   ['unbeknownst', 20],
+ ];
+ 
+ //iterating over the array to test each word
+ test.each(testCases)(
+   //name of task
+   'calculate scrabble score for %s',
+   //placeholder values
+   (word, expectedScore) => {
+      //expected outcome
+     expect(calculateScrabbleScore(word)).toBe(expectedScore);
+   }
+ );
 
